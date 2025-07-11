54b2b4614ee548369028edac15aa0b08c9240e65
System Administrator <root@Andress-MacBook-Air.local>
2025-07-11 10:24:09 -0400

    Web Version for iframe embedding - 1.4

--- scripts/bienvenida.tcl
+++ scripts/bienvenida.tcl
@@ -2,17 +2,7 @@
 
 # Función para imprimir el mensaje con estilo y emojis
 proc mensaje_bonito {} {
-    puts "🌿🌼 Hola, espero que estés disfrutando de la hora del té 🍵✨."
-    puts "Este es un proyecto humilde y bastante simplista, inspirado en mantener el cabello aceitado con algún aceite delicioso 💆‍♀️💧."
-    puts "Claro, hay gente que tiene el cabello quemado 🔥 y entonces no se puede sembrar semillas en su cabeza 🌱 porque, claro, está seco todo 😔."
-    puts "No sé qué les echan, shampoo todos los días 🧴 y después intentan que reaccione, pero no les funciona 💨 y no recibe órdenes 🚫."
-    puts "En ese caso tenemos una calculadora loca 🤪, ¡Lo que necesitamos es una calculadora cuerda! JAJAJAJAJAJ 😂."
-    puts "Yo sé, es un poco muy obvio 😅, pero todos esos feos con el pelo picho 🦱 están re locos, parce 🤯, ¡qué peligro! ⚠️"
-    puts "En fin, te invito a disfrutar de cálculos que se realizan frecuentemente en el rubro de la ingeniería eléctrica ⚡🔧, con explicaciones y ecuaciones aleatorias 📐 para ejercitar la cuestión 💡."
-    puts ""
-    puts "🌾 La palabra 'grama' se refiere a prado o hierba suave, porque ajá... 🌱"
-    puts ""
-    puts "puedes ejecutar este programa escribiendo el nombre de la terminal de TCL que es -tclsh- y seguido especificas el nombre del servicio que te interesa"
+    puts "Creditos: Kirtan Teg Singh diseño y contenidos"
 }
 
 # Llamada a la función que imprime el mensaje bonito
